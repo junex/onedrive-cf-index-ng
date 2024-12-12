c24b410c49d21c1232fe7a52adcb89e2da6c807c
songjun <songjun1991@gmail.com>
2024-12-12 11:28:20 +0800

    去掉本地调试代码

--- src/utils/kvConfigStore.ts
+++ src/utils/kvConfigStore.ts
@@ -1,13 +1,11 @@
-// import { KVNamespace } from '@cloudflare/workers-types'
+import { KVNamespace } from '@cloudflare/workers-types'
 import siteConfig from '../../config/site.config'
 
 export async function getKVConfig(): Promise<{ allowedDirectories: string[],hiddenDirectories: string[] }> {
-    // const { ONEDRIVE_CF_INDEX_KV } = process.env as unknown as { ONEDRIVE_CF_INDEX_KV: KVNamespace }
+    const { ONEDRIVE_CF_INDEX_KV } = process.env as unknown as { ONEDRIVE_CF_INDEX_KV: KVNamespace }
 
-    // const allowedDirectories = await ONEDRIVE_CF_INDEX_KV.get('allowed_directories')
-    // const hiddenDirectories = await ONEDRIVE_CF_INDEX_KV.get('hidden_directories')
-    const allowedDirectories = '公开,游戏,工具'
-    const hiddenDirectories = '公开'
+    const allowedDirectories = await ONEDRIVE_CF_INDEX_KV.get('allowed_directories')
+    const hiddenDirectories = await ONEDRIVE_CF_INDEX_KV.get('hidden_directories')
     const ceAllowedDirectories = siteConfig.allowedDirectories
         ? siteConfig.allowedDirectories.split(',').map(dir => dir.trim()).filter(Boolean)
         : []
